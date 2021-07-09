2cfcbf3c603fcce5f1b1833dfd48c3cacfbebb91
wow1186 <WOW1186@NAVER.COM>
2021-07-09 15:55:08 +0900

    simple web appliaction # add

---
+++ helloweb-ex01/app01.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const port = 8080;
+const server = http.createServer((req,resp) =>{
+       resp.writeHead(200, {
+           'Content-Type' : 'text/html'
+       });
+       resp.end(`<h1> Hello Web </h1>`)
+});
+
+server.listen(port, () => {
+   console.log(`Http Server running on port ${port}`);
+});
+
---
+++ helloweb-ex01/app04error.js
@@ -0,0 +1,38 @@
+const connect = require('connect');
+const serveStatic = require('serve-static');
+const connectRoute = require('connect-route');
+
+const port = 8080;
+const app = connect();
+app.use(serveStatic(__dirname + '/public' )); // 정적인거 다 보내줌
+
+app.use(connectRoute(function(router) {
+    
+     router.get("/" , function(req,res) {
+        res.writeHead(200 ,{
+             'Content-Type' : "text/html"
+         });
+     });
+     res.end("<h1>board view(" + req.params.no  + ") <h1>");
+     
+
+     router.get("/" , function(req,res) {
+
+        req.query= {};
+        params = req._parsedUrl-query;
+        
+        res.writeHead(200 ,{
+             'Content-Type' : "text/html"
+         });
+     });
+     res.end("<h1>board view(" + req.params.no  + ") <h1>");
+     router.get("/" , function(req,res) {
+        res.writeHead(200 ,{
+             'Content-Type' : "text/html"
+         });
+     });
+     res.end("<h1>main<h1>");
+}));
+app.listen(port,function(){
+    console.log(`Http Server running port ${port}`);
+});
